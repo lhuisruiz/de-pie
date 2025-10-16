5f74e10ac02f710909ce6970eb8cca2e03837438
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2025-10-15 22:05:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+(function() {
+  const KEY = 'installAppAccepted';
+  const alertBox = document.getElementById('install-alert');
+  const acceptBtn = document.getElementById('accept-btn');
+
+  // Mostrar alerta solo si no aceptó antes
+  if (!localStorage.getItem(KEY)) {
+    window.addEventListener('load', () => {
+      alertBox.classList.remove('hidden');
+      // Ocultar automáticamente después de 6 segundos
+      setTimeout(() => {
+        if (!localStorage.getItem(KEY)) {
+          alertBox.classList.add('hidden');
+        }
+      }, 6000);
+    });
+  }
+
+  // Si presiona Aceptar → guardar y ocultar para siempre
+  acceptBtn.addEventListener('click', () => {
+    localStorage.setItem(KEY, 'true');
+    alertBox.classList.add('hidden');
+  });
+})();
+
